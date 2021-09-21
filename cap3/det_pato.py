25389099c8a507d2b8523a186817372b459b0399
AnaV-A <ana.aguero@ug.uchile.cl>
2021-09-21 16:04:30 -0300

    modificaciones capacitacion 3

--- cap3/det_pato.py
+++ cap3/det_pato.py
@@ -13,6 +13,7 @@ from gym_duckietown.envs import DuckietownEnv
 import numpy as np
 import cv2
 
+
 def mov_duckiebot(key):
     # La acción de Duckiebot consiste en dos valores:
     # velocidad lineal y velocidad de giro
@@ -44,13 +45,13 @@ if __name__ == '__main__':
     # Definición del environment
     if args.env_name and args.env_name.find('Duckietown') != -1:
         env = DuckietownEnv(
-            seed = args.seed,
-            map_name = args.map_name,
-            draw_curve = args.draw_curve,
-            draw_bbox = args.draw_bbox,
-            domain_rand = args.domain_rand,
-            frame_skip = args.frame_skip,
-            distortion = args.distortion,
+            seed=args.seed,
+            map_name=args.map_name,
+            draw_curve=args.draw_curve,
+            draw_bbox=args.draw_bbox,
+            domain_rand=args.domain_rand,
+            frame_skip=args.frame_skip,
+            distortion=args.distortion,
         )
     else:
         env = gym.make(args.env_name)
@@ -60,8 +61,8 @@ if __name__ == '__main__':
 
     # Parametros para el detector de patos
     # Se debe encontrar el rango apropiado
-    lower_yellow = np.array([H_m, S_m, V_m])
-    upper_yellow = np.array([H_M, S_M, V_M])
+    lower_yellow = np.array([10, 200, 150])
+    upper_yellow = np.array([35, 255, 255])
     min_area = 2500
 
     while True:
@@ -86,45 +87,45 @@ if __name__ == '__main__':
 
         ### CÓDIGO DE DETECCIÓN POR COLOR ###
 
-        #Transformar imagen a espacio HSV
-
+        # Transformar imagen a espacio HSV
+        image_out = cv2.cvtColor(obs, cv2.COLOR_RGB2HSV)
 
         # Filtrar colores de la imagen en el rango utilizando
-
-
         # Bitwise-AND entre máscara (mask) y original (obs) para visualizar lo filtrado
-
+        mask = cv2.inRange(image_out, lower_yellow, upper_yellow)
+        image_out_2 = cv2.bitwise_and(obs, obs, mask=mask)
 
         # Se define kernel para operaciones morfológicas
-        kernel = np.ones((5,5),np.uint8)
+        kernel = np.ones((5, 5), np.uint8)
 
         # Aplicar operaciones morfológicas para eliminar ruido
         # Esto corresponde a hacer un Opening
         # https://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
-        #Operacion morfologica erode
-
-        #Operacion morfologica dilate
-
+        # Operacion morfologica erode
+        image_out_3 = cv2.erode(image_out_2, kernel, iterations=1)
+        # Operacion morfologica dilate
+        image_out_4 = cv2.dilate(image_out_3, kernel, iterations=1)
 
         # Busca contornos de blobs
         # https://docs.opencv.org/trunk/d3/d05/tutorial_py_table_of_contents_contours.html
-
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         # Iterar sobre contornos y dibujar bounding box de los patos
         for cnt in contours:
             # Obtener rectangulo que bordea un contorno
+            x, y, w, h = cv2.boundingRect(cnt)
 
-            #Filtrar por area minima
-            if AREA > min_area: # DEFINIR AREA
-                #Dibujar rectangulo en el frame original
-
+            # Filtrar por area minima
+            AREA = w*h
+            if AREA > min_area:  # DEFINIR AREA
+                # Dibujar rectangulo en el frame original
+                cv2.rectangle(obs, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         # Se muestra en una ventana llamada "patos" la observación del simulador
         # con los bounding boxes dibujados
         cv2.imshow('patos', cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
         # Se muestra en una ventana llamada "filtrado" la imagen filtrada
-        cv2.imshow('filtrado', image)
-
+        cv2.imshow('filtrado', cv2.cvtColor(image_out_4, cv2.COLOR_BGR2RGB))
 
     # Se cierra el environment y termina el programa
     env.close()
